8b11746ec2061a4636b4287049075e38b48803e9
Tiago Martinho <martinho@thoughtworks.com>
2017-10-10 12:14:54 +0200

    Dynamically call test method

--- sUnit/main.swift
+++ sUnit/main.swift
@@ -1,12 +1,17 @@
 class WasRun {
     
+    let testName: String
+    let testClosure: ((WasRun) -> () -> ())
+    
     var wasRun = false
     
-    init(_ testName: String) {
+    init(_ testName: String, _ testClosure: @escaping ((WasRun) -> () -> ())) {
+        self.testName = testName
+        self.testClosure = testClosure
     }
     
     func run() {
-        testMethod()
+        testClosure(self)()
     }
     
     func testMethod() {
@@ -14,7 +19,7 @@ class WasRun {
     }
 }
 
-let test = WasRun("testMethod")
+let test = WasRun("testMethod", WasRun.testMethod)
 print(test.wasRun)
 test.run()
 print(test.wasRun)
