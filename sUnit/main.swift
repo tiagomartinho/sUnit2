4b7a7e44313ce0e309858680e72938798cc5f3e3
Tiago Martinho <martinho@thoughtworks.com>
2017-10-10 11:43:23 +0200

    Initial Commit

---
+++ sUnit/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  sUnit
+//
+//  Created by Tiago Martinho on 10/10/17.
+//  Copyright © 2017 Tiago Martinho. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
